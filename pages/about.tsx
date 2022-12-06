d5707702dfd9c305168de2f119200ea8ae93440c
Geonhyuk Im <athanasia9410@gmail.com>
2022-12-06 22:43:13 +0900

    Implemented Blog Posts Page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import ContainerBlock from '../components/ContainerBlock';
 import { SiteMeta } from '../data/SiteMeta';
-
+import Image from 'next/image';
 const meta: SiteMeta = {
   title: 'About Me - Geonhyuk',
   description: 'An aspiring software engineer, based in Seoul, South Korea',
@@ -9,7 +9,35 @@ const meta: SiteMeta = {
 export default function About() {
   return (
     <>
-      <ContainerBlock metadata={meta}></ContainerBlock>
+      <ContainerBlock metadata={meta}>
+        <div>
+          <br />
+          <h1 className="text-5xl text-center">About Me</h1>
+          <br />
+          <hr className="border-1 divide-x-8 border-blue-400 drop-shadow-xl" />
+          <br />
+          <br />
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              className="rounded-full"
+              src="/images/profile.jpg"
+              height={144}
+              width={144}
+              alt="Geonhyuk Im"
+            />
+          </div>
+          <br />
+          <br />
+          <br />
+          <section className="md:container md:mx-auto">
+            <p>
+              Hello. I&#39;m Geonhyuk, call me as Geon (Gun).
+              <br />I hold a B.S in Computer Science obtained from Purdue
+              University Northwest in 2021.
+            </p>
+          </section>
+        </div>
+      </ContainerBlock>
     </>
   );
 }
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,39 +1,58 @@
 import Link from 'next/link';
 import ContainerBlock from '../components/ContainerBlock';
 import { SiteMeta } from '../data/SiteMeta';
+import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/Date';
+import { GetStaticProps } from 'next';
 
 const meta: SiteMeta = {
   title: 'My Blog Posts - Geonhyuk Im',
-  description: 'I posts my developer project experience and other random stuff',
+  description:
+    'I posts my developer project experiences and other random stuff',
 };
 
 export default function Posts({
-  allPostsData
+  allPostsData,
 }: {
   allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
+    date: string;
+    title: string;
+    id: string;
+  }[];
 }) {
-  return (<ContainerBlock metadata={meta}>    
+  return (
+    <ContainerBlock metadata={meta}>
+      <br />
+      <br />
+      <div>
+        <h1 className="text-5xl text-center">Blog Posts</h1>
+        <br />
+        <ul className="list-none list-inside">
           {allPostsData.map(({ id, date, title }) => (
-            <li className={.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+            <li className="list-item indent-8" key={id}>
+              <Link
+                href={`/posts/${id}`}
+                className="font-bold text-sky-500 text-center"
+              >
+                {title}
+              </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <p className="font-light text-black text-jusitify">
                 <Date dateString={date} />
-              </small>
+              </p>
             </li>
           ))}
-          </ContainerBlock>)   
+        </ul>
+      </div>
+    </ContainerBlock>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
-}
+      allPostsData,
+    },
+  };
+};
