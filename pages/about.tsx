d17e4dbb644cb3a8f166c7bc0826d004b1b20ecf
Geonhyuk Im <athanasia9410@gmail.com>
2022-12-10 19:22:20 +0900

    Minor fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,35 +10,35 @@ export default function About() {
   return (
     <>
       <ContainerBlock metadata={meta}>
-        <div>
-          <br />
-          <br />
-          <h1 className="font-extrabold text-4xl text-teal-700 text-center">
-            About Me
-          </h1>
-          <br />
-          <hr className="border-solid border-lime-500 ml-60 mr-60 drop-shadow-2xl" />
-          <br />
-          <br />
-          <div className="flex flex-col justify-center items-center">
-            <Image
-              className="rounded-full"
-              src="/images/profile.jpg"
-              height={144}
-              width={144}
-              alt="Geonhyuk Im"
-            />
-          </div>
-          <br />
-          <br />
-          <br />
-          <div className="md:container ml-60 mr-60 w-full">
-            <p>
-              Hello. I&#39;m Geonhyuk, call me as Geon (Gun).
-              <br />I hold a B.S in Computer Science obtained from Purdue
-              University Northwest in 2021.
-            </p>
-          </div>
+        <br />
+        <br />
+        <h1 className="font-extrabold text-4xl text-teal-700 text-center">
+          About Me
+        </h1>
+        <br />
+        <hr className="border-solid border-lime-500 ml-60 mr-60 drop-shadow-2xl" />
+        <br />
+        <br />
+        <div className="flex flex-col justify-center items-center">
+          <Image
+            className="rounded-full"
+            src="/images/profile.jpg"
+            height={144}
+            width={144}
+            alt="Geonhyuk Im"
+          />
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="md:container ml-60 mr-60 w-full">
+          <p>
+            Hello. I&#39;m Geonhyuk, call me as Geon (Gun).
+            <br />I hold a B.S in Computer Science obtained from Purdue
+            University Northwest in 2021. <br />
+            I&#39;m currently open to work. <br />
+            I&#39; most comfortable with Java and Spring Boot framework. <br />
+          </p>
         </div>
       </ContainerBlock>
     </>
