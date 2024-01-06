a1729e892695382b2d77570df1db4f3f91919e5e
Geonhyuk Im <athanasia9410@gmail.com>
2024-01-06 20:31:40 +0900

    Added Projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Blabber-Hive',
+    description: `Scalable random chat application server written in Go. It uses React TypeScript for frontend, Go Gin for backend, Postgres as database and also uses Redis and Kafka for message broker and batch processing. It is deployed on Fly.io and Clodflare pages.`,
+    imgSrc: '/static/images/projects/blabberhive.jpg',
+    href: 'https://blabberhive.com/',
   },
 ]
 
